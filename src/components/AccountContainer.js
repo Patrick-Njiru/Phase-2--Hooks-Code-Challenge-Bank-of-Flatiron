9224311834cc10ee49dbf2fe57e0528dea11dee7
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-09-28 14:09:13 +0300

    Add and use useEffect and useState React hooks. Fetch data from the backend. Pass transactions data to children.

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8001/transactions').then(res => res.json()).then(transactionData =>setTransactions(transactionData))
+    .catch(error => console.log(error))
+  })
+
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search transactions={transactions} />
+      <AddTransactionForm transactions={transactions} />
+      <TransactionsList transactions={transactions} />
     </div>
   );
 }
