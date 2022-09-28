5cb00cb93eda701e18ab853a090e0f8286f8bcf5
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-09-28 15:30:03 +0300

    Add and utilize useState hook. Create  two functions for handling added inputs. Add value attributes to inputs

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddTransactionForm() {
+  const [form, setForm] = useState({
+    date : "",
+    description: "",
+    category: "",
+    amount: 0
+  })
+
+  const handleChange = e => (setForm({ ...form, [e.target.name]: e.target.value}))
+
+  const handleSubmit  = e => {
+
+    e.preventDefault()
+    const updatedForm = {
+      date: form.date,
+      description: form.description,
+      category: form.category,
+      amount: form.amount
+    }
+
+    fetch('http://localhost:8001/transactions', {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(updatedForm)
+    })
+
+    setForm({
+      date : "",
+      description: "",
+      category: "",
+      amount: 0
+    })
+
+  }
+
   return (
     <div className="ui segment">
       <form className="ui form" onSubmit={handleSubmit}>
@@ -8,26 +42,30 @@ function AddTransactionForm() {
           <input 
           type="date" 
           name="date"
-          onChange={handleChange}
+          value={form.date}
+          onChange={(e) => handleChange(e)}
           />
           <input 
           type="text" 
           name="description" 
           placeholder="Description" 
-          onChange={handleChange}
+          value={form.description}
+          onChange={(e) => handleChange(e)}
           />
           <input 
           type="text" 
           name="category" 
           placeholder="Category"
-          onChange={handleChange} 
+          value={form.category}
+          onChange={(e) => handleChange(e)} 
           />
           <input 
           type="number" 
           name="amount" 
           placeholder="Amount" 
           step="0.01" 
-          onChange={handleChange}
+          value={form.amount}
+          onChange={(e) => handleChange(e)}
           />
         </div>
         <button className="ui button" type="submit">
