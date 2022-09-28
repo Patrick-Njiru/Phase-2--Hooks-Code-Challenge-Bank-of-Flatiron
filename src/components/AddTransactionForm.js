50419dbe28861cf7079e84920cc39e44155d44f9
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-09-28 14:31:11 +0300

    Add event listeners.

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -3,12 +3,32 @@ import React from "react";
 function AddTransactionForm() {
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input 
+          type="date" 
+          name="date"
+          onChange={handleChange}
+          />
+          <input 
+          type="text" 
+          name="description" 
+          placeholder="Description" 
+          onChange={handleChange}
+          />
+          <input 
+          type="text" 
+          name="category" 
+          placeholder="Category"
+          onChange={handleChange} 
+          />
+          <input 
+          type="number" 
+          name="amount" 
+          placeholder="Amount" 
+          step="0.01" 
+          onChange={handleChange}
+          />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
